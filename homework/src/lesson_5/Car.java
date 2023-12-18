50e597a019036f467d8d9d607bca249b6d49f015
Mykhailo Prusov <prusovmishka@gmail.com>
2023-12-18 11:40:37 +0000

    Added homework 2

---
+++ homework/src/lesson_5/Car.java
@@ -0,0 +1,21 @@
+package lesson_5;
+
+public class Car {
+    public void start(){
+        System.out.println("Start method");
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+
+    }
+    private void startElectricity(){
+        System.out.println("Starting electricity...");
+    }
+    private void startCommand(){
+        System.out.println("Start command...");
+    }
+
+    private void startFuelSystem(){
+        System.out.println("Starting fuel system...");
+    }
+}
